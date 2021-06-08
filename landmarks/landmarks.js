7677b283a6145a53bfc406217b1c4098bfd145b2
Manuel Matuzovic <manuel@matuzo.at>
2021-06-08 17:42:51 +0200

    Add landmarks script

---
+++ landmarks/landmarks.js
@@ -0,0 +1,131 @@
+// Landmarks
+
+{
+  console.clear()
+
+  console.log(`%cLandmarks`, `font-size: 13px`);
+
+  function LandMarkData(name, role, label, elem) {
+    this.name = name;
+    this.role = role;
+    this.label = label;
+  }
+
+  const results = [];
+
+  const roles = {
+    'banner': {
+      role: 'banner',
+      selectors: 'header:not([role="banner"])',
+      notWithin: 'article, aside, [role="complementary"], main, [role="main"], section, [role="region"], nav, [role="navigation"]'
+    },
+    'banner_explicit': {
+      role: 'banner',
+      selectors: '[role="banner"]',
+    },
+    'main': {
+      role: 'main',
+      selectors: '[role="main"]:not(main), main'
+    },
+    'navigation': {
+      role: 'navigation',
+      selectors: '[role="navigation"]:not(nav), nav'
+    },
+    'complementary': {
+      role: 'complementary',
+      selectors: '[role="complementary"]:not(aside), aside'
+    },
+    'form': {
+      role: 'form',
+      selectors: 'form[title], form[aria-label],  form[aria-labelledby]'
+    },
+    'search': {
+      role: 'search',
+      selectors: '[role="search"]'
+    },
+    'region': {
+      role: 'region',
+      selectors: 'section[aria-label]:not([role="region"]),  section[aria-labelledby]:not([role="region"])'
+    },
+    'region_explicit': {
+      role: 'region',
+      selectors: '[role="region"]'
+    },
+    'contentinfo': {
+      role: 'contentinfo',
+      selectors: 'footer:not([role="contentinfo"])',
+      notWithin: 'article, aside, [role="complementary"], main, [role="main"], section, [role="region"], nav, [role="navigation"]'
+    },
+    'contentinfo_explicit': {
+      role: 'contentinfo',
+      selectors: '[role="contentinfo"]'
+    },
+   }
+
+  const checkLandmark = role => {
+    const landmark = roles[role];
+    const elements = document.querySelectorAll(landmark.selectors);
+
+      for(let i = 0; i < elements.length; i++) {
+          const element = elements[i]
+          let label = ''
+
+          if (element.closest(landmark.notWithin)) {
+              continue;
+          }
+
+
+          if (element.hasAttribute('role') && element.getAttribute('role') !== landmark.role) {
+              continue;
+          }
+
+          if (element.hasAttribute('aria-label')) {
+              label = element.getAttribute('aria-label')
+          }
+
+
+          if (element.hasAttribute('aria-labelledby')) {
+              label = document.getElementById(element.getAttribute('aria-labelledby')).textContent
+          }
+
+         element.style.outline = '2px solid red';
+         element.style.outlineOffset = '-2px'
+         results.push(new LandMarkData(element, landmark.role, label))
+      }
+
+      return true;
+   }
+
+   for (role in roles) {
+      checkLandmark(role);
+   }
+
+   const checkCount = (role, element, count = 1) => {
+     const countLandmarks = results.filter(landmark => landmark.role === role).length;
+
+     if (countLandmarks > count) {
+       console.warn(`There's more than ${count} <${element}>, please check!`)
+     }
+   }
+
+  const checkLabel = (role, element, count = 1) => {
+     const countLandmarks = results.filter(landmark => landmark.role === role).length;
+     const countUnlabelled = results.filter(landmark => landmark.role === role && landmark.label === '').length;
+     if (countLandmarks > count && countUnlabelled > 0) {
+       console.warn(`There's more than ${count} <${element}> element and some are not labelled, please check!`)
+     }
+   }
+
+  checkCount('banner', 'header')
+  checkCount('main', 'main')
+  checkCount('contentinfo', 'footer')
+  checkLabel('navigation', 'nav')
+
+  console.log()
+  console.table(results)
+
+  console.log('---------')
+
+  var done = 'Finished running Landmarks”'
+  done;
+}
